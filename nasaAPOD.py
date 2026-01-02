62f02d04bd10f490d71577afd82040f8954fd32a
Robotics-now <adityaraj.imac@gmail.com>
2026-01-02 11:35:52 -0500

    Add note for .env file creation
    Added a note to create a .env file for API key.

--- nasaAPOD.py
+++ nasaAPOD.py
@@ -3,7 +3,9 @@ import streamlit as st
 from dotenv import load_dotenv
 from os import getenv
 
+# Make sure to create a .env file.
 load_dotenv()
+
 api_key = getenv('NASA_APOD_API_KEY')
 api_url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
 
