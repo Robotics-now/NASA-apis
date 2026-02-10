6d1edc75c355d66e8417219bf83c1fb213d4c487
Robotics-now <adityaraj.imac@gmail.com>
2026-02-09 19:55:43 -0500

    Update NASA APOD API call and session state handling

--- nasaAPOD.py
+++ nasaAPOD.py
@@ -4,13 +4,32 @@ import streamlit as st
 st.set_page_config(page_title="Astronomy Picture of the Day", page_icon="🔭")
 
 api_key = st.secrets["nasa"]["api_key"]
-api_url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
+api_url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}&start_date={yesterday}'
 
-request = requests.get(api_url)
+request = rq.get(api_url)
 data = request.json()
 
-st.title('Astronomy picture of the day')
-st.subheader(data.get('title', 'No title available'))
-st.image(data.get('hdurl', None))
-st.write("")
-st.write(data.get('explanation', 'No explanation available'))
+
+if "button" not in st.session_state:
+    st.session_state.button = False
+
+if st.session_state.button == False:
+    # regular APOD or today's picture
+
+    st.title('Astronomy picture of the day')
+    st.subheader(data[1]['title'])
+    st.image(data[1]['hdurl'])
+    st.write("")
+    st.write(data[1]['explanation'])
+    st.button("⇽ Yesterday's picture", on_click=lambda: st.session_state.update(button=True), key='button-y')
+
+if st.session_state.button == True:
+    # yesterday's picture
+
+    st.title('Astronomy picture of the day')
+    st.subheader(data[0]['title'])
+    st.image(data[0]['hdurl'])
+    st.write("")
+    st.write(data[0]['explanation'])
+    st.button("⇾ Today's picture", on_click=lambda: st.session_state.update(button=False), key='button-t')
+
