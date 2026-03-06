7fc0944add2f12d096391b7cc4d398609a09303c
Robotics-now <adityaraj.imac@gmail.com>
2026-03-05 21:51:58 -0500

    Refactor Today.py to improve API handling and UI

--- Today.py
+++ Today.py
@@ -2,49 +2,66 @@ import requests as rq
 import streamlit as st
 from datetime import date, timedelta
 
-yesterday = (date.today() - timedelta(days=1)).isoformat()
 
-api_key = st.secrets["nasa"]["api_key"]
-api_url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}&start_date={yesterday}'
+load_dotenv()
+yesterday = (date.today() - timedelta(days=1)).isoformat()
+today = date.today().isoformat()
+
+api_key = getenv('NASA_APOD_API_KEY')
+api_url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
 
 request = rq.get(api_url)
 data = request.json()
 
+try:
+    title = data['title']
+    url = data['hdurl']
+    explanation = data['explanation']
+    image_type = data['media_type']
+except KeyError:
+    title = data['title']
+    url = data['url']
+    explanation = data['explanation']
 
-if "button" not in st.session_state:
-    st.session_state.button = False
 
-if st.session_state.button == False:
-    # ---Regular APOD or today's picture---
-    try:
-        st.title('Astronomy picture of the day')
-        st.subheader(data[-1]['title'])
-        st.image(data[-1]['hdurl'])
-        st.write("")
-        st.write(data[-1]['explanation'])
-        st.button("⇽ Yesterday's picture", on_click=lambda: st.session_state.update(button=True), key='button-y')
 
-    except IndexError :
-    # ---Recalls the API for 1 day instead of 2---
-        
-        api_url_for_error = f'https://api.nasa.gov/planetary/apod?api_key={api_key}&date={yesterday}'
-        request_for_error = rq.get(api_url_for_error)
-        data2 = request_for_error.json()
 
-        st.title('Astronomy picture of the day')
-        st.subheader(data2['title'])
-        st.image(data2['hdurl'])
-        st.write("")
-        st.write(data2['explanation'])
-        st.button("⇽ Yesterday's picture", on_click=lambda: st.session_state.update(button=True), key='button-y')
-
-if st.session_state.button == True:
-    # ---Yesterday's picture if the usre clicks the button---
-
+if image_type == 'image':
     st.title('Astronomy picture of the day')
-    st.subheader(data[0]['title'])
-    st.image(data[0]['hdurl'])
+    st.subheader(title)
+    st.image(url)
     st.write("")
-    st.write(data[0]['explanation'])
-    st.button("⇾ Today's picture", on_click=lambda: st.session_state.update(button=False), key='button-t')
+    st.write(explanation)
+    with st.container(border=True):
+        col1, col2,  = st.columns(2)
+        
+        with col1:
+            st.page_link('pages/Previous APODs.py', label='⇽Previous APODs')
+        with col2:
+            st.download_button(
+            label="Download Image",
+            data=img_data,  # This sends the actual image bytes
+            file_name=f"{data['date']}.jpg",
+            mime="image/jpeg"
+    )
+
+elif image_type == 'video':
+    st.title('Astronomy picture of the day')
+    st.subheader(title)
+    st.video(url)
+    st.write("")
+    st.write(explanation)
+    with st.container(border=True):
+        col1, col2,  = st.columns(2)
+        
+        with col1:
+            st.page_link('pages/Previous APODs.py', label='⇽Previous APODs')
+        with col2:
+            st.download_button(
+            label="Download Image",
+            data=img_data,  # This sends the actual image bytes
+            file_name=f"{data['date']}.jpg",
+            mime="image/jpeg"
+    )
+
 
