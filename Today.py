c8c7ff9522c6f6c83427c4431a20a784904d1cb8
Robotics-now <adityaraj.imac@gmail.com>
2026-03-07 11:56:09 -0500

    Fix page icon in Streamlit configuration

--- Today.py
+++ Today.py
@@ -4,7 +4,7 @@ from datetime import date, timedelta
 from urllib.parse import urlparse
 import os
 
-st.set_page_config(page_title='Today's APOD', page_icon='🚀')
+st.set_page_config(page_title='Today's APOD', page_icon='')
 
 api_key = st.secrets["nasa"]["api_key"]
 api_url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
