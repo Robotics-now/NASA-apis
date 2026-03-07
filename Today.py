5c11237366611842f7ed11ed50e3b40ae929a91e
Robotics-now <adityaraj.imac@gmail.com>
2026-03-07 10:30:06 -0500

    Refactor API key handling and remove dotenv

--- Today.py
+++ Today.py
@@ -1,17 +1,14 @@
 import requests as rq
 import streamlit as st
-from dotenv import load_dotenv
-from os import getenv, path
 from datetime import date, timedelta
 
 
-load_dotenv()
 yesterday = (date.today() - timedelta(days=1)).isoformat()
 today = date.today().isoformat()
 img_data = rq.get(url).content
 
 
-api_key = getenv('NASA_APOD_API_KEY')
+api_key = st.secrets["nasa"]["api_key"]
 api_url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
 
 request = rq.get(api_url)
